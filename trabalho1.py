3c7dd913470756eebb0f13330616aec555656d65
fernandohcab <fernandohcab@hotmail.com>
2020-12-15 17:52:33 -0300

    Radix Sort

--- trabalho1.py
+++ trabalho1.py
@@ -1,18 +1,18 @@
 import csv
 
 # O(n²)
-def selectionSort(lista):
+def selectionSort(lista, linhas):
     for i in range(1, len(lista)):
         for j in range(0 , i):
-            if lista[i] < lista[j]:
+            if(lista[i] < lista[j]):
                 aux = lista[j]
                 lista[j] = lista[i]
                 lista[i] = aux
 
-    print(len(lista))
+    print(lista)
 
 # O(n log n)
-def shellSort(lista):
+def shellSort(lista, linhas):
     dist = len(lista) // 2
     while(dist > 0):
         i = dist
@@ -25,33 +25,41 @@ def shellSort(lista):
                 lista[j+dist] = temp
             i += 1
         dist = dist // 2
-    print(len(lista))
+    print(lista)
 
 # O(n)
-def bucket_sort(lista):
+def countingSort(arr, exp1):
+    n = len(arr)
+    output = [0] * (n)
+    count = [0] * (10)
 
-    tam = max(lista)//len(lista)
+    for i in range(0, n):
+        index = (arr[i]/exp1)
+        count[int((index)%10)] += 1
 
-    temp = []
-    resultado = []
+    for i in range(1,10):
+        count[i] += count[i-1]
 
-    for i in range(0, len(lista)):
-        temp.append([])
+    i = n-1
+    while i>=0:
+        index = (arr[i]/exp1)
+        output[ count[ int((index)%10) ] - 1] = arr[i]
+        count[int((index)%10)] -= 1
+        i -= 1
 
-    for i in range(0, len(lista)):
-        j = lista[i]//tam
-        if j != len(lista):
-            temp[j].append(lista[i])
-        else:
-            temp[len(lista) - 1].append(lista[i])
+    i = 0
+    for i in range(0,len(arr)):
+        arr[i] = output[i]
 
-    selectionSort(temp)
+def radixSort(arr, linhas):
+    max1 = max(arr)
 
-    for i in range(0, len(lista)):
-        resultado += temp[i]
+    exp = 1
+    while max1/exp > 0:
+        countingSort(arr,exp)
+        exp *= 10
 
-    print(len(resultado))
-
+    print(arr)
 
 if __name__ == "__main__":
 
@@ -63,21 +71,13 @@ if __name__ == "__main__":
 
     arquivo = open('distribuicao_respiradores.csv')
     linhas = csv.reader(arquivo)
-    for i in linhas:
-        if(i[0].lower().rstrip() == "rio de janeiro"):
-            RJ.append(i)
-        elif(i[0].lower().rstrip() == "são paulo"):
-            SP.append(i)
-        elif(i[0].lower().rstrip() == "minas gerais"):
-            MG.append(i)
-        elif(i[0].lower().rstrip() == "espirito santo"):
-            ES.append(i)
 
     print("Análise sobre a distribuição de respiradores durante a Covid-19 no Sudeste brasileiro\n")
-    print("Para analisar a data de entregas para cada Estado, digite 1")
+    print("Para analisar o número de respiradores por Estado, digite 1")
     print("Para analisar a quantidade de respiradores recebidos, digite 2")
     print("Para analisar o custo de cada entrega de respiradores, digite 3")
-    print("Para analisar o número de respiradores por Estado, digite 4")
+    print("Para analisar a data de entregas para cada Estado, digite 4")
+
     print("Para sair, aperte 0")
     n = 1
     while(n != 0):
@@ -94,30 +94,40 @@ if __name__ == "__main__":
                 print("Opção inválida. Digite uma das opções disponíveis")
                 op = int(input())
             if(op == 1):
-                lista = SP
-            elif(op == 2):
-                lista = RJ
-            elif(op == 3):
-                lista = MG
-            elif(op == 4):
-                lista = ES
-            elif(op == 5):
-                lista = SP + RJ + MG + ES
+                for i in linhas:
+                    if(i[0].lower().rstrip() == "são paulo"):
+                        lista.append(i[0])
 
-            print("Qual algoritmo deseja utilizar? \n (1) - Selection Sort \n (2) - Shell Sort \n (3) - Bucket Sort")
+            elif(op == 2):
+                for i in linhas:
+                    if(i[0].lower().rstrip() == "rio de janeiro"):
+                        lista.append(i[0])
+            elif(op == 3):
+                for i in linhas:
+                    if(i[0].lower().rstrip() == "minas gerais"):
+                        lista.append(i[0])
+            elif(op == 4):
+                for i in linhas:
+                    if(i[0].lower().rstrip() == "espirito santo"):
+                        lista.append(i[0])
+            elif(op == 5):
+                for i in linhas:
+                    lista.append(i[0])
+
+            print("Qual algoritmo deseja utilizar? \n (1) - Selection Sort \n (2) - Shell Sort \n (3) - Radix Sort")
             k = int(input())
             while(k <= 0 or k > 3):
                 print("Opção inválida. Digite uma das opções disponíveis")
                 k = int(input())
 
             if(k == 1):
-                selectionSort(lista)
+                selectionSort(lista, linhas)
                 break
             elif(k == 2):
-                shellSort(lista)
+                shellSort(lista, linhas)
                 break
             elif(k == 3):
-                bucket_sort(lista)
+                radixSort(lista, linhas)
                 break
 
         elif(n == 2):
@@ -127,30 +137,39 @@ if __name__ == "__main__":
                 print("Opção inválida. Digite uma das opções disponíveis")
                 op = int(input())
             if(op == 1):
-                lista = SP
+                for i in linhas:
+                    if(i[0].lower().rstrip() == "são paulo"):
+                        lista.append(int(i[1]))
             elif(op == 2):
-                lista = RJ
+                for i in linhas:
+                    if(i[0].lower().rstrip() == "rio de janeiro"):
+                        lista.append(int(i[1]))
             elif(op == 3):
-                lista = MG
+                for i in linhas:
+                    if(i[0].lower().rstrip() == "minas gerais"):
+                        lista.append(int(i[1]))
             elif(op == 4):
-                lista = ES
+                for i in linhas:
+                    if(i[0].lower().rstrip() == "espirito santo"):
+                        lista.append(int(i[1]))
             elif(op == 5):
-                lista = SP + RJ + MG + ES
+                for i in linhas:
+                    lista.append(int(i[1]))
 
-            print("Qual algoritmo deseja utilizar? \n (1) - Selection Sort \n (2) - Shell Sort \n (3) - Bucket Sort")
+            print("Qual algoritmo deseja utilizar? \n (1) - Selection Sort \n (2) - Shell Sort \n (3) - Radix Sort")
             k = int(input())
 
             while(k <= 0 or k > 3):
                 print("Opção inválida. Digite uma das opções disponíveis")
                 k = int(input())
             if(k == 1):
-                selectionSort(lista)
+                selectionSort(lista, linhas)
                 break
             elif(k == 2):
-                shellSort(lista)
+                shellSort(lista, linhas)
                 break
             elif(k == 3):
-                bucket_sort(lista)
+                radixSort(lista, linhas)
                 break
 
         elif(n == 3):
@@ -160,30 +179,39 @@ if __name__ == "__main__":
                 print("Opção inválida. Digite uma das opções disponíveis")
                 op = int(input())
             if(op == 1):
-                lista = SP
+                for i in linhas:
+                    if(i[0].lower().rstrip() == "são paulo"):
+                        lista.append(int(i[2]))
             elif(op == 2):
-                lista = RJ
+                for i in linhas:
+                    if(i[0].lower().rstrip() == "rio de janeiro"):
+                        lista.append(int(i[2]))
             elif(op == 3):
-                lista = MG
+                for i in linhas:
+                    if(i[0].lower().rstrip() == "minas gerais"):
+                        lista.append(int(i[2]))
             elif(op == 4):
-                lista = ES
+                for i in linhas:
+                    if(i[0].lower().rstrip() == "espirito santo"):
+                        lista.append(int(i[2]))
             elif(op == 5):
-                lista = SP + RJ + MG + ES
+                for i in linhas:
+                    lista.append(int(i[2]))
 
-            print("Qual algoritmo deseja utilizar? \n (1) - Selection Sort \n (2) - Shell Sort \n (3) - Bucket Sort")
+            print("Qual algoritmo deseja utilizar? \n (1) - Selection Sort \n (2) - Shell Sort \n (3) - Radix Sort")
             k = int(input())
 
             while(k <= 0 or k > 3):
                 print("Opção inválida. Digite uma das opções disponíveis")
                 k = int(input())
             if(k == 1):
-                selectionSort(lista)
+                selectionSort(lista, linhas)
                 break
             elif(k == 2):
-                shellSort(lista)
+                shellSort(lista, linhas)
                 break
             elif(k == 3):
-                bucket_sort(lista)
+                radixSort(lista, linhas)
                 break
 
         elif(n == 4):
@@ -192,33 +220,43 @@ if __name__ == "__main__":
             while(op <= 0 or op > 5):
                 print("Opção inválida. Digite uma das opções disponíveis")
                 op = int(input())
-            if(op == 1):
-                lista = SP
-            elif(op == 2):
-                lista = RJ
-            elif(op == 3):
-                lista = MG
-            elif(op == 4):
-                lista = ES
-            elif(op == 5):
-                lista = SP + RJ + MG + ES
+        if(op == 1):
+            for i in linhas:
+                if(i[0].lower().rstrip() == "são paulo"):
+                    lista.append(int(i[3]))
+        elif(op == 2):
+            for i in linhas:
+                if(i[0].lower().rstrip() == "rio de janeiro"):
+                    lista.append(int(i[3]))
+        elif(op == 3):
+            for i in linhas:
+                if(i[0].lower().rstrip() == "minas gerais"):
+                    lista.append(int(i[3]))
+        elif(op == 4):
+            for i in linhas:
+                if(i[0].lower().rstrip() == "espirito santo"):
+                    lista.append(int(i[3]))
+        elif(op == 5):
+            for i in linhas:
+                lista.append(int(i[3]))
 
-            print("Qual algoritmo deseja utilizar? \n (1) - Selection Sort \n (2) - Shell Sort \n (3) - Bucket Sort")
+        print("Qual algoritmo deseja utilizar? \n (1) - Selection Sort \n (2) - Shell Sort \n (3) - Radix Sort")
+        k = int(input())
+
+        while(k <= 0 or k > 3):
+            print("Opção inválida. Digite uma das opções disponíveis")
             k = int(input())
-
-            while(k <= 0 or k > 3):
-                print("Opção inválida. Digite uma das opções disponíveis")
-                k = int(input())
-            if(k == 1):
-                selectionSort(lista)
-                break
-            elif(k == 2):
-                shellSort(lista)
-                break
-            elif(k == 3):
-                bucket_sort(lista)
-                break
+        if(k == 1):
+            selectionSort(lista, linhas)
+            break
+        elif(k == 2):
+            shellSort(lista, linhas)
+            break
+        elif(k == 3):
+            radixSort(lista, linhas)
+            break
         else:
             print("Opção inválida. Digite uma das opções disponíveis")
+
     arquivo.close()
 #    print(SP)
